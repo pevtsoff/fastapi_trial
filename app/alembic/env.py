13346e639679deffe74a6c48ec462e36602b2dc3
Ivan Pevtsov <pevtsovi@cqg.com>
2022-07-20 16:31:59 +0300

    adding alembic

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,6 +8,12 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 from app.settings import settings
+import os, sys
+from app.tables import Base
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
@@ -21,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ note.md
@@ -0,0 +1,2 @@
+# Running migrations:
+alembic -c alembic.ini revision --autogenerate -m "First migration"
